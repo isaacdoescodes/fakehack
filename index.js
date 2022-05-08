b6d185cb403a4b72ceb8e5c158be7c53e3d73ad9
Isaac Boorman <iceboorman@gmail.com>
2022-05-08 16:24:07 +0100

    push

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-var app = this;
-	app.hasKeyboard = false;
-	this.keyboardPress = function() {
-		app.hasKeyboard = true;
-		window.removeEventListener("keydown", app.keyboardPress);
-	}
-	if(app.hasKeyboard = false) {
-		document.getElementById('intro').innerText = ">>> Please switch to a device which has a physical keyboard, or connect your keyboard accessory (e.g. Surface Keyboard, iPad Magic Keyboard)"
-	}
-	window.addEventListener("keydown", app.keyboardPress, false);
-	function hack() {
-		var p = document.createElement("p");
-		var outputs = [
-      
-    ];
-		var random = Math.floor(Math.random() * outputs.length);
-		var output = outputs[random];
-		p.innerHTML = output;
-		document.getElementById('outs').appendChild(p);
-		document.getElementById('intro').innerHTML = ""
-		window.scrollTo(0,document.body.scrollHeight);
-	}
-	var firstkeystroke = true;
-	window.addEventListener("keyup", function() {
-		if(firstkeystroke == true) {
-			var elem = document.documentElement;
-			if (elem.requestFullscreen) {
-				elem.requestFullscreen();
-			} else if (elem.webkitRequestFullscreen) { /* Safari */
-				elem.webkitRequestFullscreen();
-			} else if (elem.msRequestFullscreen) { /* IE11 */
-				elem.msRequestFullscreen();
-			}
-			firstkeystroke = false;
+function hack() {
+	var p = document.createElement("p");
+	var outputs = [
+		"<span style='color: yellow'>root@hackserver</span> ~ $ echo getdata('https://data.apple.com/admin?ps=GU*&@U(@@H(/getuserdata/tcook@apple.com<br><br><br>Email: tcook@apple.com<br>Name: Tim Cook<br>Data access point: https://data.apple.com/admin/user-info/uyr393hr33urr3y3<br><br><br><br><br>",
+		"<span style='color: yellow'>root@hackserver</span> ~ $ nmap https://data.apple.com/admin/user-info/uyr393hr33urr3y3?get=privateaddress<br><br>Private Billing Adress for user id=uyr393hr33urr3y3: <br><br>Tim Cook,<br> 123 Avenue Way,<br>Beverly Hills,<br>California,<br>USA<br><br><br><br><br>",
+		"<span style='color: yellow'>root@hackserver</span> ~ $ sudo apt get apple-data<br><br>Installing apple-data package via sudo apt get...<br>100% Complete<br><br><br>",
+		"<span style='color: yellow'>root@hackserver</span> ~ $ sudo apple-data --userid=uyr393hr33urr3y3<br><br><h3>Access Denied!</h3>Supsicion activtity was detected from your machine."
+	];
+	var random = Math.floor(Math.random() * outputs.length);
+	var output = outputs[random];
+	p.innerHTML = output;
+	document.getElementById('outs').appendChild(p);
+	document.getElementById('intro').innerHTML = ""
+	window.scrollTo(0, document.body.scrollHeight);
+}
+var firstkeystroke = true;
+window.addEventListener("keyup", function () {
+	if (firstkeystroke == true) {
+		var elem = document.documentElement;
+		if (elem.requestFullscreen) {
+			elem.requestFullscreen();
+		} else if (elem.webkitRequestFullscreen) { /* Safari */
+			elem.webkitRequestFullscreen();
+		} else if (elem.msRequestFullscreen) { /* IE11 */
+			elem.msRequestFullscreen();
 		}
-		window.setTimeout(hack, 1, false);
-	}, false);
+		firstkeystroke = false;
+	}
+	window.setTimeout(hack, 1, false);
+}, false);
